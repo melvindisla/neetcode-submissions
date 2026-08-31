f6a9c2dbb7946f6c50179df71915a6de4c76d5e6
Melvin Disla <Melvin.disla1@gmail.com>
2026-08-31 15:27:14 -0400

    Add: validate-parentheses - submission-30

---
+++ Data Structures & Algorithms/validate-parentheses/submission-30.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        cmap = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '['
+        }
+
+        stack = []
+        
+        for char in s:
+            if char not in cmap: # if the character is a opening bracket
+                stack.append(char)
+            elif stack and stack[-1] == cmap[char]: # if the stack is not empty and the top bracket is a match
+                stack.pop()
+            elif char in cmap: # the 
+                return False
+        return not stack
